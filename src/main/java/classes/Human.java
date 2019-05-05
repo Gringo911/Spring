2347e002376b75f7928be59f78b2238a62db2c9b
Gringo911 <irbisaltai@yandex.ua>
2019-05-05 23:03:28 +0300

    three

---
+++ src/main/java/classes/Human.java
@@ -0,0 +1,18 @@
+package classes;
+
+
+public class Human {
+    public int age;
+    public String name;
+
+    public Human(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.age + " " + this.name;
+    }
+
+}
